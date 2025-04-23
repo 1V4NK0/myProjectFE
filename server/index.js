44c186324bdb04ddb67841d23461592ff30f7007
1v4nk0 <1vancherk3s@gmail.com>
2025-04-23 13:29:27 +0100

    moved server into src folder

---
+++ server/index.js
@@ -0,0 +1,48 @@
+//importing dependencies to allow fetching and handling problems with CORS
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+//baically create new web server
+const app = express();
+
+app.use(cors()); // resolves issues with CORS
+
+// Random quote
+app.get("/api/random", async (req, res) => {
+  try {
+    const response = await fetch("https://zenquotes.io/api/random");
+    const data = await response.json();
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Quote of the day
+app.get("/api/today", async (req, res) => {
+  try {
+    const response = await fetch("https://zenquotes.io/api/today");
+    const data = await response.json();
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Inspirational image
+app.get("/api/image", async (req, res) => {
+  try {
+    const response = await fetch("https://zenquotes.io/api/image");
+    const buffer = await response.buffer();
+    res.set("Content-Type", "image/jpeg");
+    res.send(buffer);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//runs the server on port 3000
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
